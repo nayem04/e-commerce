8e1e3f2f7c094313e074eea302f8de702e57f17f
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-26 20:22:38 +0600

    Added CustomerService class

---
+++ services/customer/src/main/java/com/nayem/ecommerce/customer/CustomerService.java
@@ -0,0 +1,63 @@
+package com.nayem.ecommerce.customer;
+
+import com.nayem.ecommerce.exceptions.CustomerNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerMapper customerMapper;
+    private final CustomerRepository customerRepository;
+
+    public String createCustomer(CustomerRequest customerRequest) {
+        var customer = customerRepository.save(customerMapper.toCustomer(customerRequest));
+        return customer.getId();
+    }
+
+    public void updateCustomer(CustomerRequest customerRequest) {
+        var customer = customerRepository.findById(customerRequest.id())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        String.format("Cannot update customer:: No customer found with the provided ID: %s", customerRequest.id())
+                ));
+        mergeCustomer(customer, customerRequest);
+        customerRepository.save(customer);
+    }
+
+    private void mergeCustomer(Customer customer, CustomerRequest customerRequest) {
+        if (StringUtils.isNotBlank(customerRequest.firstname())) {
+            customer.setFirstname(customerRequest.firstname());
+        }
+        if (StringUtils.isNotBlank(customerRequest.email())) {
+            customer.setEmail(customerRequest.email());
+        }
+        if (customerRequest.address() != null) {
+            customer.setAddress(customerRequest.address());
+        }
+    }
+
+    public List<CustomerResponse> findAllCustomers() {
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::fromCustomer)
+                .collect(Collectors.toList());
+    }
+
+    public CustomerResponse findById(String id) {
+        return customerRepository.findById(id)
+                .map(customerMapper::fromCustomer)
+                .orElseThrow(() -> new CustomerNotFoundException(String.format("No customer found with the provided ID: %s", id)));
+    }
+
+    public boolean existsById(String id) {
+        return customerRepository.findById(id).isPresent();
+    }
+
+    public void deleteCustomer(String id) {
+        customerRepository.deleteById(id);
+    }
+}
