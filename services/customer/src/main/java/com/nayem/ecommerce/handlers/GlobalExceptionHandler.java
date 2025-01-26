a69964362a6626873de6e8389ab1b1b466d72bae
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-26 20:18:25 +0600

    Added a global exception handler class GlobalExceptionHandler

---
+++ services/customer/src/main/java/com/nayem/ecommerce/handlers/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.nayem.ecommerce.handlers;
+
+import com.nayem.ecommerce.exceptions.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<String> handle(CustomerNotFoundException customerNotFoundException) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(customerNotFoundException.getMsg());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        var errors = new HashMap<String, String>();
+        methodArgumentNotValidException.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var fieldName = ((FieldError) error).getField();
+                    var errorMessage = error.getDefaultMessage();
+                    errors.put(fieldName, errorMessage);
+                });
+
+        return ResponseEntity.status(BAD_REQUEST).body(new ErrorResponse(errors));
+    }
+}
