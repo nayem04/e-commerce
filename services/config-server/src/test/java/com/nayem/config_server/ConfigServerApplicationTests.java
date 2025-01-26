bba7a3b403d7ce1a5565537130d221dddb88f504
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-26 13:40:52 +0600

    Initialized config-server

---
+++ services/config-server/src/test/java/com/nayem/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayem.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
