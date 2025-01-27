3a37231185ec76395a46e33d731ef838eb03ca9d
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-27 14:56:29 +0600

    Added Category entity class

---
+++ services/product/src/main/java/com/nayem/ecommerce/category/Category.java
@@ -0,0 +1,23 @@
+package com.nayem.ecommerce.category;
+
+import com.nayem.ecommerce.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
