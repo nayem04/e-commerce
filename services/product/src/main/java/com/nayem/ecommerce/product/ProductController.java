ed6862248477b883cfe5b05ac177f166e3800b31
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-27 15:00:48 +0600

    Added ProductController class

---
+++ services/product/src/main/java/com/nayem/ecommerce/product/ProductController.java
@@ -0,0 +1,35 @@
+package com.nayem.ecommerce.product;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService service;
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(@RequestBody @Valid ProductRequest productRequest) {
+        return ResponseEntity.ok(service.createProduct(productRequest));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProducts(@RequestBody List<ProductPurchaseRequest> productPurchaseRequests) {
+        return ResponseEntity.ok(service.purchaseProducts(productPurchaseRequests));
+    }
+
+    @GetMapping("/{product-id}")
+    public ResponseEntity<ProductResponse> findById(@PathVariable("product-id") Integer productId) {
+        return ResponseEntity.ok(service.findById(productId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+}
