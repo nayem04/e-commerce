7cf4d7ee75af598a0516755b35a516d4723cb88f
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-29 13:58:39 +0600

    Initialized notification service

---
+++ services/notification/src/test/java/com/nayem/ecommerce/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayem.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
