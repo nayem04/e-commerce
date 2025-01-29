1ed3bc25568a9debe3c364719e90b270f67961d0
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-29 13:46:40 +0600

    Added OrderLineService class

---
+++ services/order/src/main/java/com/nayem/ecommerce/orderline/OrderLineService.java
@@ -0,0 +1,26 @@
+package com.nayem.ecommerce.orderline;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderLineService {
+    private final OrderLineMapper orderLineMapper;
+    private final OrderLineRepository orderLineRepository;
+
+    public Integer saveOrderLine(OrderLineRequest request) {
+        var order = orderLineMapper.toOrderLine(request);
+        return orderLineRepository.save(order).getId();
+    }
+
+    public List<OrderLineResponse> findAllByOrderId(Integer orderId) {
+        return orderLineRepository.findAllByOrderId(orderId)
+                .stream()
+                .map(orderLineMapper::toOrderLineResponse)
+                .collect(Collectors.toList());
+    }
+}
