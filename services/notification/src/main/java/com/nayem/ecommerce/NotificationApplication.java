43db8116d958ad3e6468c053ef571bdb140bba5c
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-29 20:39:17 +0600

    Enabled async

--- services/notification/src/main/java/com/nayem/ecommerce/NotificationApplication.java
+++ services/notification/src/main/java/com/nayem/ecommerce/NotificationApplication.java
@@ -2,8 +2,11 @@ package com.nayem.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class NotificationApplication {
 
     public static void main(String[] args) {
