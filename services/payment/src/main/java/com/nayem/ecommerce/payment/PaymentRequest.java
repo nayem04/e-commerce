868ede9b1abd06faf02b812730eeb3bf3326e141
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-29 13:53:03 +0600

    Added all the functionalities for payment service

---
+++ services/payment/src/main/java/com/nayem/ecommerce/payment/PaymentRequest.java
@@ -0,0 +1,13 @@
+package com.nayem.ecommerce.payment;
+
+import java.math.BigDecimal;
+
+public record PaymentRequest(
+        Integer id,
+        BigDecimal amount,
+        PaymentMethod paymentMethod,
+        Integer orderId,
+        String orderReference,
+        Customer customer
+) {
+}
