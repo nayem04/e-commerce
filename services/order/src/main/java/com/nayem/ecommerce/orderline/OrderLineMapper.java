2046a28ea6e0b823428b5d55d80925c920a76615
Mohammad Nayem <sheikhmdnayem@gmail.com>
2025-01-29 13:45:05 +0600

    Added OrderLineMapper class

---
+++ services/order/src/main/java/com/nayem/ecommerce/orderline/OrderLineMapper.java
@@ -0,0 +1,27 @@
+package com.nayem.ecommerce.orderline;
+
+import com.nayem.ecommerce.order.Order;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderLineMapper {
+    public OrderLine toOrderLine(OrderLineRequest orderLineRequest) {
+        return OrderLine.builder()
+                .id(orderLineRequest.orderId())
+                .productId(orderLineRequest.productId())
+                .order(
+                        Order.builder()
+                                .id(orderLineRequest.orderId())
+                                .build()
+                )
+                .quantity(orderLineRequest.quantity())
+                .build();
+    }
+
+    public OrderLineResponse toOrderLineResponse(OrderLine orderLine) {
+        return new OrderLineResponse(
+                orderLine.getId(),
+                orderLine.getQuantity()
+        );
+    }
+}
